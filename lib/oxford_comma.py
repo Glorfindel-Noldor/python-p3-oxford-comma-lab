8e674ab75bed68fdd8fc45d50e72f8725e722c42
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-05-13 17:29:06 -0500

    commas

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    elif len(items) == 3:
+        return f"{items[0]}, {items[1]}, and {items[2]}"
+    else:
+        comma_separated = ", ".join(items[:-1])
+        return f"{comma_separated}, and {items[-1]}"
+    
